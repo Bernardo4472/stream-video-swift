97fbf09e30480255359681db9153fca1c29ba66b
Bernardo4472 <rokaskdev@gmail.com>
2026-06-28 21:55:23 +0300

    platforms: add .macCatalyst(.v14) for Mecca Catalyst build

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "StreamVideo",
     defaultLocalization: "en",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .macCatalyst(.v14)],
     products: [
         .library(
             name: "StreamVideo",
